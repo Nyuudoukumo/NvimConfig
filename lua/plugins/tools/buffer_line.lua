62dbc6771a34e2cf3790656cdd7b0e094df88022
nyuudoukumo <1213813586@qq.com>
2025-01-15 22:32:50 +0800

    modified buffer-line shortcut

--- lua/plugins/tools/buffer_line.lua
+++ lua/plugins/tools/buffer_line.lua
@@ -3,8 +3,8 @@ return{
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function ()
-    vim.keymap.set("n","<A-h>",":BufferLineCyclePrev<CR>",opt)
-    vim.keymap.set("n","<A-l>",":BufferLineCycleNext<CR>",opt)
+    --vim.keymap.set("n","<A-h>",":BufferLineCyclePrev<CR>",opt)
+    --vim.keymap.set("n","<A-l>",":BufferLineCycleNext<CR>",opt)
     vim.opt.termguicolors = true
     require("bufferline").setup{}
   end
