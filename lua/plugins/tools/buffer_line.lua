06604ecc5a3a1a56a90286ae0f597aef9b09ac2f
fengyiting <fengyiting404@gmail.com>
2025-02-17 18:11:49 +0800

    add keymap for BufferLineMove

--- lua/plugins/tools/buffer_line.lua
+++ lua/plugins/tools/buffer_line.lua
@@ -3,9 +3,13 @@ return{
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function ()
-    --vim.keymap.set("n","<A-h>",":BufferLineCyclePrev<CR>",opt)
-    --vim.keymap.set("n","<A-l>",":BufferLineCycleNext<CR>",opt)
+    vim.keymap.set("n","<A-h>",":BufferLineMovePrev<CR>",opt)
+    vim.keymap.set("n","<A-l>",":BufferLineMoveNext<CR>",opt)
     vim.opt.termguicolors = true
-    require("bufferline").setup{}
+    require("bufferline").setup{
+      options = {
+        move_wraps_at_ends = true,
+      }
+    }
   end
 }
